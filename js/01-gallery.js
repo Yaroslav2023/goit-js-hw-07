5579903a8af0a802b780b84208e21f13c5cb8209
Yaroslav2023 <yishevchenko@ukr.net>
2023-05-29 00:30:20 +0300

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryLists = document.querySelector(".gallery");
+
+const cardsCreated = createGalleryCards(galleryItems);
+
+galleryLists.insertAdjacentHTML('afterbegin', cardsCreated);
+
+// console.log(createGalleryCards(galleryItems));
+
+function createGalleryCards(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>`
+    }).join("");
+};
+
+galleryLists.addEventListener('click', onImegesClick);
+
+function onImegesClick (event) {
+    event.preventDefault();
+    if(!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+    console.log(event.target);
+    const originalImegeRef = event.target.dataset.source;
+    console.log(originalImegeRef);
+
+    const instance = basicLightbox.create(`
+    <img src="${originalImegeRef}" width="800" height="600">
+    `);
+    instance.show();
+};
